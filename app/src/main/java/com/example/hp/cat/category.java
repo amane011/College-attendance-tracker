ed856d7c11207afa8f2a7a78e0827fa49f79d364
ankitmane11 <ankitmane11@gmail.com>
2018-11-19 21:54:07 +0530

    ready to submit

--- app/src/main/java/com/example/hp/cat/category.java
+++ app/src/main/java/com/example/hp/cat/category.java
@@ -1,9 +1,51 @@
 package com.example.hp.cat;
 
+import java.util.Date;
+
 public class category {
     private String name;
     private String email;
     private String category1;
+    private String pin;
+    private Date created;
+    private Date updated;
+    private String objectId;
+    
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    public String getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+
 
     public String getName() {
         return name;
@@ -28,4 +70,9 @@ public class category {
     public void setCategory1(String category1) {
         this.category1 = category1;
     }
+
+
+
+
+
 }
